9902b23d40914df08362f570c7d133c28bcb8ee1
kapturoff <pigunderwater@gmail.com>
2022-02-24 12:17:03 +0300

    Add important explanations

--- script.py
+++ script.py
@@ -7,9 +7,19 @@ from argparse import ArgumentParser
 RequestDetails = namedtuple("RequestDetails", "duration")
 
 parser = ArgumentParser()
-parser.add_argument("--address", "-a", type=str, required=True)
-parser.add_argument("--threads", "-t", type=int, required=True)
-parser.add_argument("--duration", "-d", type=int, default=10)
+parser.add_argument(
+    "--address", "-a", type=str, required=True, help="address of target"
+)
+parser.add_argument(
+    "--threads", "-t", type=int, required=True, help="count of threads to spam"
+)
+parser.add_argument(
+    "--duration",
+    "-d",
+    type=int,
+    default=10,
+    help="duration of running the script (in seconds)",
+)
 config = vars(parser.parse_args())
 start = time.time()
 
@@ -22,6 +32,8 @@ def spam(address: str, finish_time: float) -> int:
         requests.get(address)
         request_end = time.time_ns()
 
+        # Since time.time_ns() returns nanoseconds, which is 1/1,000,000 of millisecond,
+        # it need to divide result to 1,000,000
         requests_details.append(RequestDetails((request_end - request_start) / 1000000))
 
     return requests_details
@@ -34,20 +46,27 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=config["threads"]) as exe
 
     print(f"Starting spamming to {address}")
 
+    # Start tasks
     futures = [executor.submit(spam, address, start + duration) for _ in range(threads)]
 
+    # When the tasks are completed
     for future in concurrent.futures.as_completed(futures):
         result = future.result()
-        total_requests_sent += len(result)
+        total_requests_sent += len(result)  # How many request a thread sent
+
+        # Getting the average speed from all requests made by the thread
         averages_request_times.append(
             sum(map(lambda request_detail: request_detail.duration, result))
             / len(result)
         )
 
-    print(f"Finished! {total_requests_sent} requests sent in {duration} seconds.")
-    print(
-        f"Average speed was {round(total_requests_sent / duration)} requests per second."
+    average_all_threads_speed = round(total_requests_sent / duration)
+    average_single_request_speed = sum(averages_request_times) / len(
+        averages_request_times
     )
+
+    print(f"Finished! {total_requests_sent} requests sent in {duration} seconds.")
+    print(f"Average speed was {average_all_threads_speed} requests per second.")
     print(
-        f"Every thread was spending around {sum(averages_request_times) / len(averages_request_times)} ms for request."
+        f"Every thread was spending around {average_single_request_speed} ms for request."
     )
